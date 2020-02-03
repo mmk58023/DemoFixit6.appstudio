321be55e3b551769ce5d2b9c3ddd105c8e253407
Maya <mmk58023@creighton.edu>
2020-02-03 16:23:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DemoFixit6.appstudio
+ this is a class demo we did
---
+++ forms/Welcome/Welcome.js
@@ -0,0 +1,9 @@
+let name = "Bob"
+let names = ["Bob", "Mary", "Sue"]
+
+
+
+
+btnShowMessage.onclick=function(){
+ console.log(`My best friend is ${names[0]}`) 
+}
